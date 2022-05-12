0c9cddcb27a4e70fe21996a8dc8febd35245a9f3
asaf arviv <asaf.arviv@consumrz.com>
2022-05-12 06:13:06 +0200

    sql

---
+++ src/main/resources/Bank_mysql_create.sql
@@ -0,0 +1,68 @@
+drop schema if exists bankdb;
+
+create schema bankdb;
+
+use bankdb;
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL,
+	birth_date DATETIME NOT NULL,
+	phone_number VARCHAR(255) NOT NULL,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL,
+	created_by DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE users_auth (
+	id INT NOT NULL AUTO_INCREMENT,
+	owner INT NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	created_at DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE loans (
+	id INT NOT NULL AUTO_INCREMENT,
+	owner INT NOT NULL ,
+	loan_balance INT NOT NULL,
+	created_at DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE credit_expenses (
+	id INT NOT NULL AUTO_INCREMENT,
+	owner INT NOT NULL,
+	created_at DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE pass_back_operations (
+	id INT NOT NULL AUTO_INCREMENT,
+	owner INT NOT NULL ,
+	isCharge BOOLEAN NOT NULL,
+	account_balance FLOAT NOT NULL,
+	created_at DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE foreign_Trade (
+	id INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE users_auth ADD CONSTRAINT users_auth_fk0 FOREIGN KEY (owner) REFERENCES users(id);
+
+ALTER TABLE loans ADD CONSTRAINT loans_fk0 FOREIGN KEY (owner) REFERENCES users(id);
+
+ALTER TABLE credit_expenses ADD CONSTRAINT credit_expenses_fk0 FOREIGN KEY (owner) REFERENCES users(id);
+
+ALTER TABLE pass_back_operations ADD CONSTRAINT pass_back_operations_fk0 FOREIGN KEY (owner) REFERENCES users(id);
+
+
+
+
+
+
